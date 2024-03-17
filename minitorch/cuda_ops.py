3cdd53200da5264866ce9abb9c59c12f0f63bc40
Victor Guerra <vguerra@gmail.com>
2024-03-17 12:11:14 +0100

    fix matrix multiply in CUDA backend.
    By fixing conditions on copies from a and b storages to the respective
    shared block memory.

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -440,32 +440,33 @@ def _tensor_matrix_multiply(
     #    b) Copy into shared memory for b matrix
     #    c) Compute the dot produce for position c[i, j]
 
-    out_pos = batch * out_strides[0] +  j * out_strides[-2] + i * out_strides[-1]
-
-    a_row_offset = a_strides[-2]
-    b_row_offset = b_strides[-2]
-
     tmp = numba.float64(0.)
-    for b in range((BLOCK_DIM + a_shape[-1] - 1) / BLOCK_DIM):
-        # all threads work first in loading into shared memory
-        # data equivalent to a block size
-        a_shared[pj, pi] = numba.float64(0.)
-        b_shared[pj, pi] = numba.float64(0.)
-        b_x = BLOCK_DIM * b + pi
-        b_y = BLOCK_DIM * b + pj
 
-        if pj < a_shape[-2] and b_x < a_shape[-1]:
-            a_storage_idx = batch * a_batch_stride + pj * a_row_offset + b_x * a_strides[-1]
-            a_shared[pj, pi] = a_storage[a_storage_idx]
+    common_dim_size = a_shape[2]
+    out_pos = batch * out_strides[0] +  i * out_strides[1] + j * out_strides[2]
+    a_pos = batch * a_batch_stride + i * a_strides[1] + pj * a_strides[2]
+    b_pos = batch * b_batch_stride + pi * b_strides[1] + j * b_strides[2]
 
-        if b_y < b_shape[-2] and pi < b_shape[-1]:
-            b_storage_idx = batch * b_batch_stride + b_y * b_row_offset +  pi * b_strides[-1]
-            b_shared[pj, pi] = b_storage[b_storage_idx]
+    for b in range(0, common_dim_size, BLOCK_DIM):
+        a_shared[pi, pj] = numba.float64(0.)
+        b_shared[pi, pj] = numba.float64(0.)
+
+        # checks to do:
+        # - for a_shared : BLOCK_DIM + pj < cols (common_dim_size)
+        # - for b_shared : BLOCK_DIM + pi < rows (common_dim_size)
+        if (b + pj) < common_dim_size:
+            a_shared[pi, pj] = a_storage[a_pos]
+        if (b + pi) < common_dim_size:
+            b_shared[pi, pj] = b_storage[b_pos]
 
         cuda.syncthreads()
 
-        for k in range(cuda.blockDim.x):
-            tmp += a_shared[pj, k] * b_shared[k, pi]
+        if (out_pos < out_size):
+            for k in range(BLOCK_DIM):
+                tmp += a_shared[pi, k] * b_shared[k, pj]
+
+        a_pos += BLOCK_DIM * a_strides[2]
+        b_pos += BLOCK_DIM * b_strides[1]
 
         cuda.syncthreads()
 
